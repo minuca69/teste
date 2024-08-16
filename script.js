54e59e457ba3f29af96ae0b14386dd6700d59a7d
Manuel Gonzalez <87993163072@senacrs.edu.br>
2024-08-16 11:09:54 -0300

    index html

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = prompt("Digite seu nome");
+alert("Olá "+nome+", tudo bem");
+
+
+
+//
+
+console.log("Teste de console");
+
+//
+
+document.getElementById("demo").innerHTML = "I am the better"
